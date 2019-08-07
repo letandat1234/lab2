0895f732f9774e2c423d5fb9b14cb825767c7d5e
letandat1234 <datltgcd17091@fpt.edu.vn>
2019-08-07 15:33:49 +0700

    Add project files.

---
+++ lab2/MvcPWx/ASPSMSX2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MvcPWx
+{
+    internal class ASPSMSX2
+    {
+        internal class ASPSMSX2SoapClient
+        {
+            private string v;
+
+            public ASPSMSX2SoapClient(string v)
+            {
+                this.v = v;
+            }
+
+            internal void SendSimpleTextSMS(string v1, string v2, string destination, string v3, string body)
+            {
+                throw new NotImplementedException();
+            }
+
+            internal void Close()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
